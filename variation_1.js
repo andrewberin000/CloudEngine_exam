c9d2083e73598af37f5fdb14d9223305bd6356f9
hannaclaros000 <hannaclaros000@gmail.com>
2017-08-14 19:56:18 +0800

    updated_exam01

--- variation_1.js
+++ variation_1.js
@@ -9,7 +9,7 @@ if (mq.matches) {
   $('head').append('<style>input {outline: none; margin: 8px 3px!important;width:100%;}.section-content.section-content--narrow p {margin-bottom: 17px; }<style>'); }
 
 // adding new buttons
-$('.tabbed-overview__copy').after('<input type="button" class="catteg" id="getStronger" value="Get Stronger"/><input type="button" class="catteg" id="getLearner"value="Get Learner"/><input type="button" class="catteg" id="getFaster" value="Get Faster"/>');
+$('.tabbed-overview__copy').after('<input type="button" class="catteg" id="getStronger" value="Get Stronger"/><input type="button" class="catteg" id="getLeaner"value="Get Leaner"/><input type="button" class="catteg" id="getFaster" value="Get Faster"/>');
 
 //All les mills workouts" to "what is your fitness goal"
 $('.tabbed-overview__title').text('What is your fitness goal?');
@@ -18,7 +18,7 @@ $('.tabbed-overview__copy').text('From flexibility to HIIT and everything inbetw
 //adding ID of Parent categories and class of each category.---filtering class.
 $("ul.class-list__list").attr("id","lessMillscategories");
 $(".class-list__item:nth-child(4), .class-list__item:nth-child(1), .class-list__item:nth-child(13)").addClass("getStronger");
-$(".class-list__item:nth-child(2), .class-list__item:nth-child(3), .class-list__item:nth-child(5),.class-list__item:nth-child(6), .class-list__item:nth-child(7), .class-list__item:nth-child(8), .class-list__item:nth-child(8), .class-list__item:nth-child(10), .class-list__item:nth-child(11)").addClass("getLearner");
+$(".class-list__item:nth-child(2), .class-list__item:nth-child(3), .class-list__item:nth-child(5),.class-list__item:nth-child(6), .class-list__item:nth-child(7), .class-list__item:nth-child(8), .class-list__item:nth-child(8), .class-list__item:nth-child(10), .class-list__item:nth-child(11)").addClass("getLeaner");
 $(".class-list__item:nth-child(12), .class-list__item:nth-child(14), .class-list__item:nth-child(15), .class-list__item:nth-child(16), .class-list__item:nth-child(17), .class-list__item:nth-child(18), .class-list__item:nth-child(19), .class-list__item:nth-child(20)").addClass("getFaster");
 
 //fitness goals list----filtered
